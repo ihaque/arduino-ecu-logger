14e1b008947a562bfd8f4a8b3a78e68f464affa5
Imran Haque <ihaque@cs.stanford.edu>
2012-10-07 03:03:04 -0700

    Oppa functional style

--- python/can-dumper.py
+++ python/can-dumper.py
@@ -1,7 +1,7 @@
-from time import time
+from functools import partial
 from sys import argv
 from sys import stdin
-from itertools import cycle
+from time import time
 
 from arduino import ArduinoSource
 from text_log import TextSource
@@ -18,16 +18,28 @@ else:
         return len(inrdy) > 0
 
 
+def check_keyboard():
+    if kbhit():
+        while kbhit():
+            stdin.read(1)
+        raise KeyboardInterrupt
+    return
+
+
+def broadcast(sinks, frame):
+    for sink in sinks:
+        sink.writeFrame(frame)
+        check_keyboard()
+    return
+
+
 def main():
     source = ArduinoSource("COM7")
     sinks = [CursesSink(25, 80), LogSink(argv[1])]
-    for frame in source:
-        for sink in sinks:
-            sink.writeFrame(frame)
-            if kbhit():
-                while kbhit():
-                    stdin.read(1)
-                return
+    try:
+        map(partial(broadcast, sinks), source)
+    except KeyboardInterrupt:
+        pass
 
 
 if __name__ == "__main__":
